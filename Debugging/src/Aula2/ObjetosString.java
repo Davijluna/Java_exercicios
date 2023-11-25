822dd4afa8a66353fa60e36efb8a3f2eb13d0293
davi <davijluna@gmail.com>
2023-11-25 14:34:33 -0300

    feature/ Adicionando arquivo das Strins.

---
+++ Debugging/src/Aula2/ObjetosString.java
@@ -0,0 +1,9 @@
+package Debugging.src.Aula2;
+
+public class ObjetosString {
+  public static void main(String[] args) {
+    String frase = "Estou aprendendo Java!";
+    String numeros = "09890";
+    String caractere = "a";
+  }
+}
