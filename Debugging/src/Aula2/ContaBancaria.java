fd967c66945e0fc84620ecb08e1590143937b454
davi <davijluna@gmail.com>
2023-11-25 11:37:59 -0300

    feature/ Chamando os métodos na classe Banco utilizando o objeto que armazenamos na variável conta.

--- Debugging/src/Aula2/ContaBancaria.java
+++ Debugging/src/Aula2/ContaBancaria.java
@@ -19,5 +19,10 @@ public class ContaBancaria {
 public class Banco {
   public static void main(string[] args) {
     ContaBancaria conta = new ContaBancaria();
+
+    conta.mostraMensagem();
+    conta.depositar(500);
+
+    double valorConvertido = conta.converteMoeda(30, 5);
   }
 }
