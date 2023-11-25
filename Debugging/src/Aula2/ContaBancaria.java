b0513b77f44c0ceecf770a75b0a225460629ab19
davi <davijluna@gmail.com>
2023-11-25 11:32:44 -0300

    feature/ Adicionando três métodos.

--- Debugging/src/Aula2/ContaBancaria.java
+++ Debugging/src/Aula2/ContaBancaria.java
@@ -1,6 +1,18 @@
 package Debugging.src.Aula2;
 
 public class ContaBancaria {
+  void mostraMensagem() {
+    System.out.println("Conta bancária acessada.");
+  }
+
+  void depositar(double valor) {
+    System.out.println("Valor depositado: ");
+    System.out.println(valor);
+  }
+
+  double converteMoeda(float total,float cambio) {
+    return total * cambio;
+  }
 }
 
 
