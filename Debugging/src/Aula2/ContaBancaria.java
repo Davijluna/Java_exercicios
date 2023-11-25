a33ea625d95e5ddea8827b9673525eecb2111430
davi <davijluna@gmail.com>
2023-11-25 11:02:12 -0300

    feature/ instanciando um objeto em Java

--- Debugging/src/Aula2/ContaBancaria.java
+++ Debugging/src/Aula2/ContaBancaria.java
@@ -1,5 +1,11 @@
 package Debugging.src.Aula2;
 
 public class ContaBancaria {
+}
 
+
+public class Banco {
+  public static void main(string[] args) {
+    ContaBancaria conta = new ContaBancaria();
+  }
 }
