072588ad6c8ebef696945b151a96287eaa26780f
davi <davijluna@gmail.com>
2023-11-26 22:48:04 -0300

    feature/ Adicionando pela segunda vez conteúdo a class para testar.

--- Debugging/src/Aula2/ClassesWrapper.java
+++ Debugging/src/Aula2/ClassesWrapper.java
@@ -4,7 +4,10 @@ public class ClassesWrapper {
   public static void main(String[] args) {
     Integer valor = 7;
 
-    int outroValor = valor;
-  }
+//    int outroValor = valor;
 
+    System.out.println(
+        Integer.toBinaryString(valor)
+    );
+  }
 }
