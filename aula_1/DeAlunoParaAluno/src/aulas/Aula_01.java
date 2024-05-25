8ffeb5fb4cde979562ae263e4ea1b9cdef3916ea
davi <davijluna@gmail.com>
2024-05-25 17:51:40 -0300

    feat: inicio aula 1

---
+++ aula_1/DeAlunoParaAluno/src/aulas/Aula_01.java
@@ -0,0 +1,10 @@
+package aulas;
+
+
+// Class
+public class Aula_01 {
+    // metodo
+    public static void main(String[] args) { // args : argumento
+        System.out.println("Olâ Mundo");
+    }
+}
