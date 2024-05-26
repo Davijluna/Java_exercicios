b06079bf1cf28a92d495adf622cf3136f5ce9f5e
davi <davijluna@gmail.com>
2024-05-26 16:15:04 -0300

    feat:  utilizando variaveis para armazenar um valor int

--- Conteudos/Fundamentos/src/helloworld/Helloworld.java
+++ Conteudos/Fundamentos/src/helloworld/Helloworld.java
@@ -8,8 +8,8 @@ public class Helloworld {
     public static void main(String[] args) {
         System.out.println("Hello World. ");
         
-        System.out.println("Primeira linha");
-        System.out.println("Segunda linha");
+        System.out.println(" 1° Primeira linha");
+        System.out.println(" 2° Segunda linha");
     }
     
 }
