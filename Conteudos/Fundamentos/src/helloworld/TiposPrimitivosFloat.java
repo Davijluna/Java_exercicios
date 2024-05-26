6f3b46032325cd9c8fd22a07415e971a2e34c37a
davi <davijluna@gmail.com>
2024-05-26 17:03:56 -0300

    feat: tipo double

--- Conteudos/Fundamentos/src/helloworld/TiposPrimitivosFloat.java
+++ Conteudos/Fundamentos/src/helloworld/TiposPrimitivosFloat.java
@@ -1,6 +1,13 @@
 package helloworld;
 
 
+/*
+No Java, é necessário adicionar a letra f no final do número para indicar que 
+ele é do tipo float. O motivo é que a precisão entre float 
+e double são diferentes. Se não inserirmos a letra f, o Java entenderá que você
+está tentando atribuir um valor do tipo double a uma variável do tipo float.
+*/
+
 public class TiposPrimitivosFloat {
     
     public static void main(String[] args) {
