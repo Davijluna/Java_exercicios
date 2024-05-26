f7e5386ae9767d0d33636a4989ec8cbc52642b98
davi <davijluna@gmail.com>
2024-05-26 16:39:05 -0300

    feat: utilizando variavel int

---
+++ Conteudos/Fundamentos/src/helloworld/TiposPrimitivosInt.java
@@ -0,0 +1,16 @@
+package helloworld;
+
+
+public class TiposPrimitivosInt {
+    
+    public static void main(String[] args) {
+        int numeroInteiro = 10;
+        int a = 10, b = 20, soma;
+        
+        soma = a + b;
+        
+        System.out.println(numeroInteiro);
+        System.out.println(soma);
+    }
+    
+}
