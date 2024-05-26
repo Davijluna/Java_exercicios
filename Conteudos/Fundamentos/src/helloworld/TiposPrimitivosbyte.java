ca54e0ffbd24e05e5e1fe442baa2be49a54f531c
davi <davijluna@gmail.com>
2024-05-26 16:23:18 -0300

    feat: utilizando byte

---
+++ Conteudos/Fundamentos/src/helloworld/TiposPrimitivosbyte.java
@@ -0,0 +1,13 @@
+package helloworld;
+
+
+public class TiposPrimitivosbyte {
+    
+    public static void main(String[] args) {
+        
+        byte b = 9;
+        
+        System.out.println(b);
+    }
+    
+}
