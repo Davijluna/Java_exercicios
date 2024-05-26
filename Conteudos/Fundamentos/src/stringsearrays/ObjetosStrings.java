1cba4a9b177d2604fb03081289031960816b715c
davi <davijluna@gmail.com>
2024-05-26 20:18:04 -0300

    arrays e strings

---
+++ Conteudos/Fundamentos/src/stringsearrays/ObjetosStrings.java
@@ -0,0 +1,11 @@
+package stringsearrays;
+
+
+public class ObjetosStrings {
+    public static void main(String[] args) {
+        String frase = "Estou aprendendo Java";
+        String numero = "09890";
+        String caractere = "a";
+    }
+    
+}
