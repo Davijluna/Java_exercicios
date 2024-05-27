bb87ce0f17f7f3465e9ff7b4018cbed94de2b77b
davi <davijluna@gmail.com>
2024-05-27 19:52:42 -0300

    feat: Debug

---
+++ Conteudos/Debugging/src/debugging/Debug.java
@@ -0,0 +1,11 @@
+package debugging;
+
+
+public class Debug {
+    
+    public static void main(String[] args) {
+         NumerosImpares impares = new NumerosImpares();
+         impares.imprimir();
+    }
+    
+}
