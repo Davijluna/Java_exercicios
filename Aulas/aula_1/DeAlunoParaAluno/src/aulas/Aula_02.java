19158bab24b871b138707a749450048e6473847b
davi <davijluna@gmail.com>
2024-05-26 23:39:25 -0300

    feat: variaveis JAVA

---
+++ Aulas/aula_1/DeAlunoParaAluno/src/aulas/Aula_02.java
@@ -0,0 +1,11 @@
+package aulas;
+
+public class Aula_02 {
+    
+    public static void main(String[] args) {
+        
+        int num = 20;
+        
+        System.out.println("Você tem " + num +" anos");
+    }
+}
