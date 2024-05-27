b14310a3bacb8b7157611e461703d84d93b7a94d
davi <davijluna@gmail.com>
2024-05-27 09:21:38 -0300

    feat: estrutura For

---
+++ Conteudos/ControleDeFluxoEntradaDeDados/src/estruturaCondidionais/For.java
@@ -0,0 +1,19 @@
+package estruturaCondidionais;
+
+
+public class For {
+    
+    public static void main(String[] args) {
+        
+        int numero;
+        String log;
+        
+        for (numero = 1; numero < 10; numero += 1) {
+            
+            log = "Numero é igual a " + numero + "\n";
+            System.out.println(log);
+            
+        }
+    }
+    
+}
