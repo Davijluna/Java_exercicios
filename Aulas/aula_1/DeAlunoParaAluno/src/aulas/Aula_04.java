c2be6ac92b2a01948a4a691d13d0cc15bc81f7a7
davi <davijluna@gmail.com>
2024-05-26 23:57:32 -0300

    feat: aula 4 tipo double

---
+++ Aulas/aula_1/DeAlunoParaAluno/src/aulas/Aula_04.java
@@ -0,0 +1,18 @@
+package aulas;
+
+
+public class Aula_04 {
+    
+    public static void main(String[] args) {
+        int x;
+        x = 150;
+        
+        int y = 4;
+        
+        double resultado = x / y;
+        
+        
+        System.out.println(resultado);
+    }
+    
+}
