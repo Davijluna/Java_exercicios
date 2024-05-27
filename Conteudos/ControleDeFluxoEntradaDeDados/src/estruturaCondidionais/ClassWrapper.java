e1596c2607e5c5e27bed26271381b0dbaa853df5
davi <davijluna@gmail.com>
2024-05-27 18:43:23 -0300

    feat: convertendo string em numero

---
+++ Conteudos/ControleDeFluxoEntradaDeDados/src/estruturaCondidionais/ClassWrapper.java
@@ -0,0 +1,21 @@
+package estruturaCondidionais;
+
+
+public class ClassWrapper {
+    
+      public static void main(String[] args) {
+        Integer valor = 7;  // autoboxing (autoempacotamento)
+
+        int outroValor = valor; // unboxing (desempacotamento)
+  }
+      
+      
+//        public static void main(String[] args) {
+//            int valor = 42;
+//
+//            System.out.println(
+//            Integer.toBinaryString(valor)
+//            );
+//  }
+    
+}
