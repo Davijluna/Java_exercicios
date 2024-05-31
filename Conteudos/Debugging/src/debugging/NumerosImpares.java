d3b6d85f7bad686790b7d58c8ae9315e0f63f4d0
davi <davijluna@gmail.com>
2024-05-30 23:58:55 -0300

    refactor Numeros Impares

--- Conteudos/Debugging/src/debugging/NumerosImpares.java
+++ Conteudos/Debugging/src/debugging/NumerosImpares.java
@@ -9,6 +9,7 @@ public class NumerosImpares {
         public static void imprimir() {
         for (int i = 1; i <= 10; i++) {
         if (i % 2 == 1) {
+     // if (i % 2 != 0) <<< SOLUÇÃO DO GABARITO.
         System.out.println(i);
       }
     }
