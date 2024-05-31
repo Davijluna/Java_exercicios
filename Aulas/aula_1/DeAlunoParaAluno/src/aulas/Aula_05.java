ca9444e4c0d7be8c96329b64cae499401154ea82
davi <davijluna@gmail.com>
2024-05-31 00:17:32 -0300

    feat: aual 05

--- Aulas/aula_1/DeAlunoParaAluno/src/aulas/Aula_05.java
+++ Aulas/aula_1/DeAlunoParaAluno/src/aulas/Aula_05.java
@@ -1,13 +1,21 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package aulas;
 
-/**
- *
- * @author davi
- */
 public class Aula_05 {
+    
+    public static void main(String[] args) {
+        
+     /*   byte a =100;// numeros pequeno
+        short b; // nemeros medios
+        int c; // numeros grandes
+        long d; // muemros enormes
+
+       float e; // precisão menor
+        double f; // precesão maior
+
+       boolean g;  // false ou true
+       char h; // caracteres*/
+        
+        System.out.println("Tipos de dados primitivos em Java");
+    }
     
 }
