39f0809138042dd2839807c596f91e3327df12dd
davi <davijluna@gmail.com>
2024-05-30 23:56:30 -0300

    refactor: modificando conta bancaria

--- Conteudos/Fundamentos/src/conta/ContaBancaria.java
+++ Conteudos/Fundamentos/src/conta/ContaBancaria.java
@@ -5,8 +5,9 @@ public class ContaBancaria {
     // Este é um atrubuto.
     double saldo = 0;
     
+//  construtor
      public ContaBancaria(double saldo) {
-    this.saldo = saldo;
+        this.saldo = saldo;
   }
     
     void mostrarmensagen() {
@@ -20,11 +21,7 @@ public class ContaBancaria {
     
     double convertermoeda(float total, float cambio){
         return total * cambio;
-    }
-    
-    
-    
-    
+    }   
 }
 
 
