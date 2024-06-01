5718107ed377e07632ce05cc1f5f7590d0da9aae
davi <davijluna@gmail.com>
2024-06-01 13:52:14 -0300

    feat: aula 10 / casting

---
+++ Aulas/aula_1/DeAlunoParaAluno/src/aulas/Aula_10.java
@@ -0,0 +1,26 @@
+package aulas;
+
+/**
+ *
+ * @author davi
+ */
+public class Aula_10 {
+    
+    public static void main(String[] args) {
+        
+        int x = 10;
+        int y = 6; int z = 3;
+        double a = 3; double b = 1.0;
+        double c = (double)(x / y); // casting
+        int h = (int)c;
+        
+        System.out.println("x = "+ h);
+        System.out.println("y = "+ y);
+        System.out.println("z = " + z );
+        
+        System.out.println("a = " + a);
+        System.out.println("b = "+ b);
+        System.out.println("c = " + c);
+    }
+    
+}
