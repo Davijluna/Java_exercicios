6ddbb0e5355d9b981012245c32513dc0d111fdf8
davi <davijluna@gmail.com>
2024-06-10 11:27:45 -0300

    feat: adicionando exemplo de POO

---
+++ Conteudos/POO/src/main/java/televisao/Television.java
@@ -0,0 +1,11 @@
+package televisao;
+
+/**
+ *
+ * @author davi
+ */
+public class Television {
+    public Television() {
+        System.out.println("Construtor da class Television sendo executado.");
+    }
+}
