f34e7002300d8457678bc94dbd421b96750c8f52
davi <davijluna@gmail.com>
2024-06-11 23:08:59 -0300

    criando classe playablechacacter

---
+++ Conteudos/POO/src/main/java/heranca/PlayableCharacter.java
@@ -0,0 +1,22 @@
+package heranca;
+
+public class PlayableCharacter {
+  private String name; // nome
+  private String race; // raça (elfo, humano, orc)
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getRace() {
+    return race;
+  }
+
+  public void setRace(String race) {
+    this.race = race;
+  }
+}
