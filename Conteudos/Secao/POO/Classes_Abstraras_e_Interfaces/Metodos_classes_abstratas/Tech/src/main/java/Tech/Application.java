4d80a8d7db960e6f90ff6f81075456ff58829e27
davi <davijluna@gmail.com>
2024-07-19 16:37:52 -0300

    feat: métodos e classes abstratas e classes abstratas e polimorfismo

---
+++ Conteudos/Secao/POO/Classes_Abstraras_e_Interfaces/Metodos_classes_abstratas/Tech/src/main/java/Tech/Application.java
@@ -0,0 +1,18 @@
+package Tech;
+
+public class Application {
+
+  public static void main(String[] args) {
+    Laptop laptop = new Laptop("Samsung", 100, 14);
+    Desktop desktop = new Desktop("Dell", 500);
+
+    checkDevice(laptop);
+    checkDevice(desktop);
+//    laptop.bootUp();
+  }
+
+  public static void checkDevice(ComputerDevice device) {
+    System.out.println("Verificando dispositivo de marca " + device.getBrand());
+    device.bootUp();
+  }
+}
---
+++ Conteudos/Secao/POO/Classes_Abstraras_e_Interfaces/Metodos_classes_abstratas/Tech/src/main/java/Tech/Laptop.java
@@ -0,0 +1,16 @@
+package Tech;
+
+public class Laptop extends ComputerDevice{
+  private double size;  // tamanho (14", 15", etc)
+
+  public Laptop(String brand, double storageCapacity, double size) {
+    super(brand, storageCapacity);
+
+    this.size = size;
+  }
+
+  @Override
+  public void bootUp() {
+    System.out.println("Inicializando o laptop de tamanho " + size + "...");
+  }
+}
