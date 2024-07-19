3fc051bafaaec070dfaac9316d60f9598c9d69ee
davi <davijluna@gmail.com>
2024-07-19 12:41:56 -0300

    retirando arquivo

--- Conteudos/POO/src/main/java/SobrescritaDeMetodos/Mage.java
+++
@@ -1,9 +0,0 @@
-package SobrescritaDeMetodos;
-
-public class Mage extends PlayableCharacter{
-
-  @Override
-  public void attack() {
-    System.out.println("O mago lança um feitiço.");
-  }
-}
--- Conteudos/POO/src/main/java/SobrescritaDeMetodos/Warrior.java
+++
@@ -1,9 +0,0 @@
-package SobrescritaDeMetodos;
-
-public class Warrior extends PlayableCharacter{
-
-  @Override
-  public void attack() {
-    System.out.println("O guerreiro ataca com sua arma.");
-  }
-}
