fa259f6c1d768aaba74f2119b0db89cbfb40e433
davi <davijluna@gmail.com>
2024-07-25 22:08:42 -0300

    comentário

--- Conteudos/Secao/POO/Classes_Abstraras_e_Interfaces/Metodos_classes_abstratas/Tech/src/main/java/Tech/QuantumComputer.java
+++ Conteudos/Secao/POO/Classes_Abstraras_e_Interfaces/Metodos_classes_abstratas/Tech/src/main/java/Tech/QuantumComputer.java
@@ -4,6 +4,7 @@ public abstract class QuantumComputer extends ComputerDevice {
   public QuantumComputer(String brand, double storageCapacity) {
     super(brand, storageCapacity);
   }
-// teste
+// teste 
+// contar aula de video
   public abstract void quantumComputation();
 }
