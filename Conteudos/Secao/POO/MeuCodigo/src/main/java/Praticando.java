8c9820004a0914f2e0d4690b5e21157359594df8
davi <davijluna@gmail.com>
2024-10-01 23:11:15 -0300

    feat: soma de valores em uma lista

---
+++ Conteudos/Secao/POO/MeuCodigo/src/main/java/Praticando.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Praticando {
+  public static void main(String[] args) {
+    List<Integer> numeros = new ArrayList<>();
+    numeros.add(1);
+    numeros.add(2);
+    numeros.add(3);
+    numeros.add(4);
+    numeros.add(5);
+    numeros.add(6);
+    numeros.add(7);
+    int soma = 0;
+    double media;
+
+    List<Integer> quadradosPares = new ArrayList<>();
+    for (Integer numero : numeros) {
+      soma += numero;
+    }
+    media = (double) soma / numeros.size();
+    for (Integer numero : numeros) {
+      if (numero % 2 == 0) {
+        quadradosPares.add(numero * numero);
+      }
+    }
+    quadradosPares.forEach(System.out::println);
+    System.out.println(soma);
+    System.out.println(media);
+  }
+}
