0fae7327ca7a26362d12228e366c66d0c990a5fa
davi <davijluna@gmail.com>
2024-10-07 23:10:20 -0300

    feat: recebendo uma lista de números pares

--- Conteudos/Secao/POO/MeuCodigo/src/main/java/Exercicio.java
+++ Conteudos/Secao/POO/MeuCodigo/src/main/java/Exercicio.java
@@ -1,3 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Exercicio {
+  public static void main(String[] args) {
+    List<Integer>  numeros = new ArrayList<>();
+    numeros.add(3);
+    numeros.add(4);
+    numeros.add(8);
+    numeros.add(17);
+    numeros.add(32);
+
+//    System.out.println(numeros);
+    List<Integer> pares = new ArrayList<>();
+    for (Integer numero : numeros) {
+      if (numero % 2 == 0) {
+        pares.add(numero);
+      }
+
+    }
+    System.out.println(pares);
+  }
 
 }
